08292522786e18ad1e32f91f7a1441cb71e6d354
vanita22 <vanitayrafita2004@gmail.com>
2020-10-21 00:19:11 -0400

    subiendo index-perfil para monica

--- js/UI.js
+++ js/UI.js
@@ -1,77 +1,70 @@
 export default class UI {
-    getUser(){
-        fetch(this.url,{
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json', 'Accept': 'application/json'
-            },
-            body: JSON.stringify(data),
-        })
-        .then(response => response.json())
-        .then(data => printUsers(data))
-    }
-
-    putUser(){
-        fetch(this.url)
-        .then(response => response.json())
-        .then(data => this.addUsers(data.users));
-    }
-
-    printUsers(user) {
-        const tableBody = document.getElementById('datos');
-        tableBody.innerHTML = '';
-        user.forEach((us) => {
-            const row = `<p>${us.id}</p>
-                        <p>${us.name}</p>
-                        <p>${us.email}</p>`
-            tableBody.innerHTML += row;
-        })        
-    }
-
-    // // Guardar
-    // addCars() {
-    //     //se captura el dato del input
-    //     const marca = document.getElementById('marca').value;  
-
-    //     if(marca && modelo && color && año && precio !== '') {
-    //         // Creo un nuevo usuario que es un objeto
-    //         const newCars = {
-    //             id: cars.length + 1,
-    //             marca: marca
-    //         }  
-    //         // Agrego el usuario creado al array que tiene todos los usuarios
-    //         cars.push(newCars);
-    //         // imprimo nuevamente a todos los usuarios
-    //         printUsers();
-    //         document.getElementById('marca').value='';
-    //     }else {
-    //         alert("no se puede dejar campos vacios");
-    //     }        
+    // getUser(){
+    //         fetch(this.url)
+    //         .then(response => response.json())
+    //         .then(data => printUser(data))        
     // }
 
-    // // Editar
-    // submitCar() {
-    //     let editingCar = false;
-    //     console.log(editingCar)
-    //     if(editingCar) {
-    //         editCar();
-    //     } else {
-    //         addCars();
-    //     }
-    // }
-    // editCar() {
-    //     let editingCar = false;
-    //     const marca = document.getElementById('marca').value;
-    //     editingCar.marca = marca
-    //     printUsers();
-    //     editingCar = false;
-    //     document.getElementById('marca').value = '';
+    // putUser(){
+    //     fetch(this.url)
+    //     .then(response => response.json())
+    //     .then(data => this.addUsers(data.users));
     // }
 
-    // editCarButton(id) {
-    //     const carr = cars.find((car) => car.id === id);  
-    //     document.getElementById('marca').value = carr.marca;
-    //     editingCar = carr;
-    // }    
-    getUser;
+    // printUser(user){
+    //     const tableBody = document.getElementById('datos');
+    //     tableBody.innerHTML = '';
+    //     user.forEach(us => {
+    //         const row = `<p>${us['id']}</p>
+    //                     <p>${us['name']}</p>
+    //                     <p>${us['email']}</p>`
+    //         tableBody.innerHTML += row;
+    //     }); 
+    // }
+
+    // // // Guardar
+    // // addCars() {
+    // //     //se captura el dato del input
+    // //     const marca = document.getElementById('marca').value;  
+
+    // //     if(marca && modelo && color && año && precio !== '') {
+    // //         // Creo un nuevo usuario que es un objeto
+    // //         const newCars = {
+    // //             id: cars.length + 1,
+    // //             marca: marca
+    // //         }  
+    // //         // Agrego el usuario creado al array que tiene todos los usuarios
+    // //         cars.push(newCars);
+    // //         // imprimo nuevamente a todos los usuarios
+    // //         printUsers();
+    // //         document.getElementById('marca').value='';
+    // //     }else {
+    // //         alert("no se puede dejar campos vacios");
+    // //     }        
+    // // }
+
+    // // // Editar
+    // // submitCar() {
+    // //     let editingCar = false;
+    // //     console.log(editingCar)
+    // //     if(editingCar) {
+    // //         editCar();
+    // //     } else {
+    // //         addCars();
+    // //     }
+    // // }
+    // // editCar() {
+    // //     let editingCar = false;
+    // //     const marca = document.getElementById('marca').value;
+    // //     editingCar.marca = marca
+    // //     printUsers();
+    // //     editingCar = false;
+    // //     document.getElementById('marca').value = '';
+    // // }
+
+    // // editCarButton(id) {
+    // //     const carr = cars.find((car) => car.id === id);  
+    // //     document.getElementById('marca').value = carr.marca;
+    // //     editingCar = carr;
+    // // }    
 }
--- js/perfil.js
+++ js/perfil.js
@@ -1,56 +1,84 @@
-import Datos from './Datos.js';
-import UI from './UI.js';
+// import Datos from './Datos.js';
+// import UI from './UI.js';
 
 
-document.getElementById('perfil').addEventListener('submit', (event) => {   
-    event.preventDefault();
+// document.getElementById('perfil').addEventListener('submit', (event) => {   
+//     event.preventDefault();
 
-    // const urlGetUSer = new Datos ('https://matter-app.herokuapp.com/api/v1/users');
-    // const ui = new UI();
-    // ui.getUser(urlGetUSer);    
-});
+//     const urlGetUSer = new Datos ('https://matter-app.herokuapp.com/api/v1/users');
+//     const ui = new UI();
+//     ui.getUser(urlGetUSer); 
+       
+// });
+// function getUser(){
+//     fetch('https://matter-app.herokuapp.com/api/v1/users')
+//     .then(response => response.json())
+//     .then(data => printUser(data))        
+// }
+// getUser();
 
+// function userId(data){
+//     let idUs = data.forEach(re => {
+//         let row = re['id']; 
+//         return row;                                   
+//     });    
+
+//     console.log(idUs);
+
+//     // fetch(`https://matter-app.herokuapp.com/api/v1/users/${idUs}`)
+//     //     .then(response => response.json())
+//     //     .then(data => console.log(data)); 
+// }
+// userId;
+
+// function putUser(){
+//     fetch(`'https://matter-app.herokuapp.com/api/v1/users/${userId()}'`)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+// }
+// putUser();
 function getUser(){
     fetch('https://matter-app.herokuapp.com/api/v1/users')
     .then(response => response.json())
-    .then(data => printUser(data))
+    .then(data => printUser(data))        
 }
+getUser();
 
 function printUser(user){
-    const tableBody = document.getElementById('datos');
-        tableBody.innerHTML = '';
-        user.forEach((us) => {
-            const row = `<p>${us['id']}</p>
-                        <p>${us['name']}</p>
-                        <p>${us['email']}</p>`
-            tableBody.innerHTML += row;
-        }) 
+    // const tableBody = document.getElementById('users');
+    // const mailLog =  document.getElementById('email-login').value;
+    tableBody.innerHTML = '';
+    user.forEach(re => {
+        let row = `<p>${re.name}</p>
+                <p>${re.email}</p>
+                <button type="submit" onclick="editUser(${re.id})">Editar</button>`
+        tableBody.innerHTML += row;                                
+    }); 
 }
-printUser()
-getUser();
-// function openFile(event) {
-//   var input = event.target;
+// printUser();
 
-//   var reader = new FileReader();
-//   reader.onload = function(){
-//     var dataURL = reader.result;
-//     var output = document.getElementById('output');
-//     output.src = dataURL;
-//   };
-//   reader.readAsDataURL(input.files[0]);
-// };
+// Guardar
+function editUser(id) {
+     alert(id);  
+}
+editUser;
 
-// var openFile = function(event) {
-//   var input = event.target;
+function openFile(event) {
+    var input = event.target;
 
-//   var reader = new FileReader();
-//   reader.onload = function(){
-//     var dataURL = reader.result;
-//     var output = document.getElementById('output');
-//     output.src = dataURL;
-//   };
-//   reader.readAsDataURL(input.files[0]);
-// };
+    var reader = new FileReader();
+    reader.onload = function(){
+      var dataURL = reader.result;
+      var output = document.getElementById('output');
+      output.src = dataURL;
+    };
+    reader.readAsDataURL(input.files[0]);
+};
 
-// openFile;
+openFile;
+
+window.openFile = openFile;
+
+window.editUser = editUser;
+
 
