32068a45b4d40b76f3e254c22447a4c76b437d41
Mariangel <mariangelacosta.88@gmail.com>
2020-10-21 18:49:35 -0500

    Se añade la funcionalidad del login

--- JS/login.js
+++ JS/login.js
@@ -1,4 +1,5 @@
 function login() {
+    let statusCode;
     const URL = 'https://matter-app.herokuapp.com/api/v1/auth/login';
     const METHOD = 'POST'
     const inputEmail = document.getElementById('input-email').value;
@@ -15,15 +16,18 @@ function login() {
       headers: {
         'Content-Type': 'application/json'
       },
-    }).then(response => response.json())
+    }).then(response => {
+        statusCode = response.status
+        return  statusCode == 200 ? response.json() : undefined;
+    })
     .catch(error => console.error('Error:', error))
-    .then(data => checkCredentials(data))
+    .then(data => checkCredentials(data, statusCode))
 }
 
-function checkCredentials(response){
+function checkCredentials(response, statusCode){
 
-    if(response == undefined){
-        alert('No user found for this email/password');
+    if(statusCode != 200){
+        alert('Credenciales inválidas o usuario no existe. Por favor verificar');
     } else {
         window.location.replace("./home.html");
         sessionStorage.setItem('id', response.id);   
