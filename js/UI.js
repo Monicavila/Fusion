1c267aee1924446b2fdc7eb04f1c78d2a88a1a43
vanita22 <vanitayrafita2004@gmail.com>
2020-10-22 17:52:36 -0400

    cargando nuevos cambios con poo

--- js/UI.js
+++ js/UI.js
@@ -1,70 +1,136 @@
-export default class UI {
-    // getUser(){
-    //         fetch(this.url)
-    //         .then(response => response.json())
-    //         .then(data => printUser(data))        
-    // }
+export default class Ui {
+    constructor(url) {
+        this.url = url;
+    }  
+    // esta función inicia sesión
+    log() {
+        const emailUser = document.getElementById('input-email').value;
+        const passUser = document.getElementById('input-psw').value;
 
-    // putUser(){
-    //     fetch(this.url)
-    //     .then(response => response.json())
-    //     .then(data => this.addUsers(data.users));
-    // }
+        const user = {email:  emailUser, password: passUser}
+        console.log(user)
+        // const user = {email:  'julirg58@gmail.com', password: '12345678'}
+        fetch(this.url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', 'Acept': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then((response) => response.json())
+        .then((data) => {this.setIdUser(data)
+            this.printUser(data)})   
+    }
 
-    // printUser(user){
-    //     const tableBody = document.getElementById('datos');
-    //     tableBody.innerHTML = '';
-    //     user.forEach(us => {
-    //         const row = `<p>${us['id']}</p>
-    //                     <p>${us['name']}</p>
-    //                     <p>${us['email']}</p>`
-    //         tableBody.innerHTML += row;
-    //     }); 
-    // }
+    // recibe el usuario que inició sesión y guardamos el usuario completo en el sessionStorage y retornamos el id del usuario
 
-    // // // Guardar
-    // // addCars() {
-    // //     //se captura el dato del input
-    // //     const marca = document.getElementById('marca').value;  
+    setIdUser(data) {
+        sessionStorage.setItem('id', data.id)
+    }
 
-    // //     if(marca && modelo && color && año && precio !== '') {
-    // //         // Creo un nuevo usuario que es un objeto
-    // //         const newCars = {
-    // //             id: cars.length + 1,
-    // //             marca: marca
-    // //         }  
-    // //         // Agrego el usuario creado al array que tiene todos los usuarios
-    // //         cars.push(newCars);
-    // //         // imprimo nuevamente a todos los usuarios
-    // //         printUsers();
-    // //         document.getElementById('marca').value='';
-    // //     }else {
-    // //         alert("no se puede dejar campos vacios");
-    // //     }        
-    // // }
+    getIdUser() {
+        return JSON.parse(sessionStorage.getItem('id'))
+    }
 
-    // // // Editar
-    // // submitCar() {
-    // //     let editingCar = false;
-    // //     console.log(editingCar)
-    // //     if(editingCar) {
-    // //         editCar();
-    // //     } else {
-    // //         addCars();
-    // //     }
-    // // }
-    // // editCar() {
-    // //     let editingCar = false;
-    // //     const marca = document.getElementById('marca').value;
-    // //     editingCar.marca = marca
-    // //     printUsers();
-    // //     editingCar = false;
-    // //     document.getElementById('marca').value = '';
-    // // }
+    getUser(){
+        
+        const idNewUser = getIdUser();
+        
+        var urlencoded = {name: document.getElementById('name').value};
+        console.log(urlencoded)   
 
-    // // editCarButton(id) {
-    // //     const carr = cars.find((car) => car.id === id);  
-    // //     document.getElementById('marca').value = carr.marca;
-    // //     editingCar = carr;
-    // // }    
+        fetch(`https://matter-app.herokuapp.com/api/v1/users/${idNewUser}`,{
+                
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json', 'Acept': 'application/json'
+            },
+            body: JSON.stringify(urlencoded),
+            // redirect: 'follow'    
+        })
+        .then(response => response.json())
+        .then(data => this.printUser(data));
+    }
+
+    printUser(user){
+        const tableBody = document.getElementById('users');
+        tableBody.innerHTML = '';
+        let row = `<p>Nombre: ${user.name}</p>
+            <p>Email: ${user.email}</p>
+            <p>Password: ${user.password}</p>
+            <p>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    Mostrar
+                </button>
+            </p>
+            <div id="clearUser" onsubmit="event.preventDefault(), saveUser()">
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body">
+                        <label for="name">nombre</label>
+                        <input type="text" id="name">
+                        <button class="btn btn-primary" type="button" onclick="editUser(${user.id})">Editar</button>
+                    </div>
+                </div>
+            </div>`
+        tableBody.innerHTML += row; 
+    }
+
+    // Guardar
+    addUser() {
+        let idEdit = false;
+        //se captura el dato del input
+        const name = document.getElementById('name').value;  
+        if(name !== '') {
+            // Creo un nuevo usuario que es un objeto
+            const newUser = {
+                name: name
+            }  
+            // Agrego el usuario creado al array que tiene todos los usuarios
+            user.push(newUser);
+            // imprimo nuevamente a todos los usuarios
+            printUsers();
+            document.getElementById('name').value='';
+        }else {
+            alert("no se puede dejar campos vacios");
+        }    
+    }
+
+    editingUser() {
+        let idEdit = false;
+        const name = document.getElementById('name').value;
+        idEdit.name = name
+        printUsers();
+        idEdit = false;
+        document.getElementById('name').value = '';
+    }
+
+    editUser(id) {
+        let idEdit = false;
+        const us = id;  
+        document.getElementById('name').value = us.name;
+        idEdit = us;
+    }
+
+    submitUser() {
+        let idEdit = false;
+        console.log(idEdit)
+        if(idEdit) {
+            editUser();
+        } else {
+            addUser();
+        }
+    }
+
+    // Para cargar la imagen
+    openFile(event) {
+        var input = event.target;
+
+        var reader = new FileReader();
+        reader.onload = function(){
+        var dataURL = reader.result;
+        var output = document.getElementById('output');
+        output.src = dataURL;
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
 }
--- js/perfil.js
+++ js/perfil.js
@@ -1,129 +1,14 @@
-// import Datos from './Datos.js';
-// import UI from './UI.js';
+import Ui from './UI.js';
 
 
-// document.getElementById('perfil').addEventListener('submit', (event) => {   
-//     event.preventDefault();
+document.getElementById('mostrar').addEventListener('click', (event) => {   
+    event.preventDefault();
 
-//     const urlGetUSer = new Datos ('https://matter-app.herokuapp.com/api/v1/users');
-//     const ui = new UI();
-//     ui.getUser(urlGetUSer); 
-       
-// });
-
-
-
-// let idEdit = false;
-
-function getUser(){
-    fetch('https://matter-app.herokuapp.com/api/v1/users')
-    .then(response => response.json())
-    .then(data => userId(data))        
-}
-getUser();
-
-function userId(data){
-    let mailLog =  document.getElementById('email-login').value;
+    document.getElementById('imgMostrar').addEventListener('click', (event) => event.openFile());
     
-    data.forEach(re => {
-        if(re.email === mailLog) {
-            // let newID = re.id;
-            // console.log(newID)
-            alert("usuario encontrado");
-        }                                 
-    });    
-}
-userId;
-
-
-// function oneUser(){
-//     fetch(`'https://matter-app.herokuapp.com/api/v1/users/${userId()}'`)
-//     .then(response => response.json())
-//     .then(data => printUser(data));
-// }
-// oneUser();
-
-// function printUser(user){
-//     const tableBody = document.getElementById('users');
-//     // let mailLog =  document.getElementById('email-login').value;
-//     tableBody.innerHTML = '';
-
-//     user.forEach(re => {
-//         // if(mailLog === re.mail){
-//             let row = `<p>${re.name}</p>
-//                 <p>${re.email}</p>
-//                 <p>
-//                     <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-//                         Mostrar
-//                     </button>
-//                 </p>
-//                 <div id="clearUser" onsubmit="event.preventDefault(), saveUser()">
-//                     <div class="collapse" id="collapseExample">
-//                         <div class="card card-body">
-//                             <label for="name">nombre</label>
-//                             <input type="text" id="name">
-//                             <button class="btn btn-primary" type="button" onclick="editUser(${re.id})">Editar</button>
-//                         </div>
-//                     </div>
-//                 </div>`
-//             tableBody.innerHTML += row; 
-//         // }                               
-//     }); 
-// }
-//  printUser;
-
-// //Editar
-// function editUser(id) {
-//     const edUser = re.filter(re => re.id === id)
-//     const inputName = document.getElementById('name'); //en estos input se ponen los valores
-//     inputName.value = edUser.name; //poner los imput los valores que se van a editar
-//     idEdit = edUser.id;
-//     console.log(edUser);
-//     console.log(id);
-// }
-// editUser;
-
-// // //guardar
-// function saveUser() {
-//     const newName = document.getElementById('name').value;//obtiene los nuevos valores para guardar
-//     if(idEdit === false) {
-//         alert('selecciona el nombre a editar');
-//         return;
-//     }
-//     //actualiza el array de usuarios
-//     const edUser = user.find(re => re.id === idEdit)
-//     edUser.name = newName;
-
-//     //para limpiar el input
-//     document.getElementById('clearUser').reset();
-
-//     //actualiza la vista
-//     printUser();
-//     console.log(user);
-// }
-
-
-
-
-
-// Para cargar la imagen
-function openFile(event) {
-    var input = event.target;
-
-    var reader = new FileReader();
-    reader.onload = function(){
-      var dataURL = reader.result;
-      var output = document.getElementById('output');
-      output.src = dataURL;
-    };
-    reader.readAsDataURL(input.files[0]);
-};
-
-openFile;
-
-window.openFile = openFile;
-// window.printUser = printUser;
-// window.editUser = editUser;
-window.userId = userId;
-
+    const ui = new Ui('https://matter-app.herokuapp.com/api/v1/auth/login');
+    ui.log(); 
+    // ui.log(pass); 
+          
+});
 
