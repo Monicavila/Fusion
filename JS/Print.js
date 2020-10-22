df4691bfae3af862db9e64443cf1d1a9d4dc929c
Monicavila <di.monicavila@gmail.com>
2020-10-22 16:05:36 -0500

    Reload page bug

--- JS/Print.js
+++ JS/Print.js
@@ -38,25 +38,25 @@ export default class Print{
         let middleHtml='';
         skills.forEach((skill,index) => {
             middleHtml+=`            <div >
-                                        <div class="card-body">
+                                        <div class="card-body bg-light" style="padding: 10px;">
                                             <h5 class="card-title">${skill.name}</h5>
                                             <h6 class="card-subtitle">${email}</h6>
                                             <p class="card-text">Califica de 1 a 5 .</p>
                                             <div class="btn-group btn-group-toggle" data-toggle="buttons">
                                                 
-                                                    <label id="loption1-${index}" for="option1-${index}"  class="btn btn-outline-primary m-1">
+                                                    <label id="loption1-${index}" for="option1-${index}"  class="btn btn-outline-info m-1">
                                                     <input onchange="change('1','${index}')" type="radio" name="option-${skill.id}-${invitationId}" id="option1-${index}" required >1 
                                                     </label>
-                                                    <label id="loption2-${index}" for="option2-${index}" class="btn btn-outline-primary m-1">
+                                                    <label id="loption2-${index}" for="option2-${index}" class="btn btn-outline-info m-1">
                                                     <input onchange="change('2','${index}')" type="radio" name="option-${skill.id}-${invitationId}" id="option2-${index}" required>2
                                                     </label>
-                                                    <label id="loption3-${index}" class="btn btn-outline-primary m-1">
+                                                    <label id="loption3-${index}" class="btn btn-outline-info m-1">
                                                     <input onchange="change('3','${index}')" type="radio" name="option-${skill.id}-${invitationId}" id="option3-${index}" required>3
                                                     </label>
-                                                    <label id="loption4-${index}" class="btn btn-outline-primary m-1">
+                                                    <label id="loption4-${index}" class="btn btn-outline-info m-1">
                                                     <input onchange="change('4','${index}')" type="radio" name="option-${skill.id}-${invitationId}" id="option4-${index}" required>4
                                                     </label>
-                                                    <label id="loption5-${index}" class="btn btn-outline-primary m-1">
+                                                    <label id="loption5-${index}" class="btn btn-outline-info m-1">
                                                     <input onchange="change('5','${index}')" type="radio" name="option-${skill.id}-${invitationId}" id="option5-${index}"required>5
                                                     </label>
                                               
@@ -65,7 +65,7 @@ export default class Print{
                                     </div>`
             
         });
-        allSkills.innerHTML=starHtml+middleHtml+`<button type="submit" class="btn btn-primary m-2"> ENVIAR </button></form>`;
+        allSkills.innerHTML=starHtml+middleHtml+`<button type="submit" class="btn btn-info m-2"> ENVIAR </button></form>`;
         
        
                                     
