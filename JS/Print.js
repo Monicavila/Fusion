fe00f125b07b0ade4886bb4c6ef71b809aaba220
Monicavila <di.monicavila@gmail.com>
2020-10-22 15:03:20 -0500

    Feedback styles

--- JS/Print.js
+++ JS/Print.js
@@ -10,11 +10,11 @@ export default class Print{
             onlyValidate.forEach(invitation=>{
                 const info=r[1].find(element=>element.id==invitation.user_id)
                 
-                allPersons.innerHTML+=`<div class="card col-md-4 col-sm-12 m-2" style="width: 18rem;">
+                allPersons.innerHTML+=`<div class="card bg-light col-md-6 col-sm-12 col-xs-12 m-2" style="width: 18rem; display: inline;">
                                             <div class="card-body">
-                                                <h5 class="card-title">${info.email}</h5>
-                                                <p class="card-text">Te ha enviado una petición de feedback, pulsa el botón para realizarla.</p>
-                                                <a href="#" onclick="callSkills('${invitation.id}','${info.email}')" class="btn btn-primary">FEEDBACK</a>
+                                                <h6 class=" card-title">${info.email}</h6>
+                                                <p class="card-text text-justify">Te ha enviado una petición de feedback, pulsa el botón para realizarla.</p>
+                                                <a href="#" onclick="callSkills('${invitation.id}','${info.email}')" class="btn btn-info">FEEDBACK</a>
                                             </div>
                                         </div>`
                 
