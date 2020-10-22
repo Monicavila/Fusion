bb9026804553c7810e7da969cd9dacde5494827a
Monicavila <di.monicavila@gmail.com>
2020-10-22 18:26:02 -0500

    add perfil

---
+++ JS/perfil.js
@@ -0,0 +1,11 @@
+import Ui from './UI.js';
+
+
+document.getElementById('mostrar').addEventListener('click', (event) => {   
+    event.preventDefault();
+
+    document.getElementById('imgMostrar').addEventListener('click', (event) => event.openFile());
+    
+    const ui = new Ui('https://matter-app.herokuapp.com/api/v1/auth/login');
+    ui.log();           
+});
