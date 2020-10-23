ca9f0aa801b79a2905acf1b31427c25bf8a0f80d
Edwin Alberto <leonora.ea@gmail.com>
2020-10-23 15:05:26 -0500

    Update test

--- rFeedback/Apli.js
+++ rFeedback/Apli.js
@@ -1,83 +1,35 @@
-// feedback
-// -> getALl()
-// ui
-// -> printFeedback(feedback)
-// user
-// ->loggeIn
-
-// const ui = new UI();
-// const user = new user();
-// user.loggedInd();
-// const feedBackObject = new feedBack()
-// const feedback = feedBackObject.getAll(user.loggedIn);
-// ui.printFeedback(feedback)
-
-
 function ready(){
-    const urlFeedback = 'https://matter-app.herokuapp.com/api/v1/users/83/invitations';
+    const urlFeedback = 'https://matter-app.herokuapp.com/api/v1/users/86/invitations';
     fetch(urlFeedback)
         .then((response) => response.json())
-        .then((data) => {printItemName(data); console.log(data);})
+        .then((data) => {printItemName(data)}) // --------
 }
-//  function nameUser(data){
-//      const test = data;
-//      let nameUser = [ ];
-//      return new Promise((resolve, reject) => {
-//          data.forEach(d => {
-//              const urlNameUser = 'https:matter-app.herokuapp.com/api/v1/users/'+ d.user_invited_id + '';
-//              fetch(urlNameUser)
-//                  .then((response) => response.json())
-//                  .then((data) => nameUser.push(data))    
-//          });
-//          resolve(joint(data, nameUser))
-//      })  
-//  }
-//  function joint(data, user){
-//      let jointName = [ ];
-//      let element;
-//      let newName = 0;
-//      console.log(data);
-//      console.log(user);
-    
-//     // setTimeout(( ) =>{
-//     // console.log(user); 
-//     // },2000);
-
-//     setTimeout(( ) =>{
-//         for (let i = 0; i < data.length; i++) {
-//             do{
-//                 console.log(newName);
-//                 if( user[newName].id == data[i]["user_invited_id"]){
-//                     console.log("Id -" +user[newName].id);
-//                     jointName.push(user[newName]["name"]);
-//                     user.splice(newName, 1);
-//                     break;
-//                 }
-//                 newName++;
-//             }while(newName == data.length)
-//         }
-//         // console.log(jointName);
-//     },3000);
-//  }
 function printItemName(data){
     e = 0;
     let containerName = document.getElementsByClassName('content-Name')[0];   
     containerName.innerHTML = " ";
+    if(data.length > 0){ // ----
         data.forEach(d => {     
-                let b = d.skills;
-                if( b.length > 1 ){
-                    let cardName = ` <div onclick="ready(), printItem(${e})" class="item-list">
-                                <span> ${e} - ${d.user_invited.name} </span> 
-                            </div>`;
-                    containerName.innerHTML += cardName;
-                }else{
-                    let cardName = ` <div onclick="" class="item-list inactive">
-                                <span> ${e} - ${d.user_invited.name} </span> 
-                            </div>`;
-                    containerName.innerHTML += cardName;
-                }
-                e++;        
+            let b = d.skills;
+            if( b.length > 1 ){
+                let cardName = ` <div onclick="ready(), printItem(${e})" class="item-list">
+                            <span> ${e} - ${d.user_invited.name} </span> 
+                        </div>`;
+                containerName.innerHTML += cardName;
+            }else{
+                let cardName = ` <div onclick="" class="item-list inactive">
+                            <span> ${e} - ${d.user_invited.name} </span> 
+                        </div>`;
+                containerName.innerHTML += cardName;
+            }
+            e++;        
         });
+    }else { // Z--------
+        let cardName = ` <div onclick="" class="item-list inactive"> 
+                            <span> :( - ¡Aún no aceptan tus invitaciónes de Feedback! </span> 
+                        </div>`;
+                containerName.innerHTML += cardName;
+    } // --------Z   
     printFeedback(data)
 }
 
@@ -91,12 +43,32 @@ function printFeedback(data){
     let i = validItem;
     let containerItem = document.getElementsByClassName('carousel-inner')[0];
     containerItem.innerHTML = " ";
-    setTimeout(( ) =>{
-        data[i].skills.forEach( e => {
-            let item = itemFeedback(e); 
-            containerItem.innerHTML += item;                       
-        });
-    },500);
+    if(data.length > 0 ){ // ----
+        setTimeout(( ) =>{
+            data[i].skills.forEach( e => {
+                let item = itemFeedback(e); 
+                containerItem.innerHTML += item;                       
+            });
+        },500);    
+    }else{ // z---
+            let item = '';
+            let active = ["active"];
+            for (let i = 0; i < 3; i++){
+                item += `<div class="carousel-item ${active[i]}">
+                <img src="photo_${i+1}.jpg" class="d-block w-100" alt="photo_${i}" title="test">
+                <div class="carousel-caption d-none d-md-block line">
+                    <h5> Test </h5>
+                    <span class="material-icons"> military_tech </span>
+                    <span class="material-icons iconDisabled"> military_tech </span>
+                    <span class="material-icons iconDisabled"> military_tech </span>
+                    <span class="material-icons iconDisabled"> military_tech </span>
+                    <span class="material-icons iconDisabled"> military_tech </span>
+                </div>
+            </div>`;    
+            }
+            
+        containerItem.innerHTML += item;                       
+    } // ----Z
 }
 
 function itemFeedback (skill){
@@ -136,14 +108,14 @@ function itemFeedback (skill){
                 img = 3;
                 active = " ";
             }
-let item = `<div class="carousel-item ${active}">
-            <img src="photo_${img}.jpg" class="d-block w-100" alt="photo_${img}" title="${name}">
-            <div class="carousel-caption d-none d-md-block line">
-                <h5> ${name} </h5>
-                ${valueItem}
-            </div>
-        </div>`;
-return item;
+    let item = `<div class="carousel-item ${active}">
+                <img src="photo_${img}.jpg" class="d-block w-100" alt="photo_${img}" title="${name}">
+                <div class="carousel-caption d-none d-md-block line">
+                    <h5> ${name} </h5>
+                    ${valueItem}
+                </div>
+            </div>`;
+    return item;
 }
 
 ready();
